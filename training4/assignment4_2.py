19d816935d2b84057b7e17993085de30d03354bf
Jorgen Hauge <jorgen78@gmail.com>
2024-11-27 15:47:56 +0100

    Fix path to plot file

--- training4/assignment4_2.py
+++ training4/assignment4_2.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pathlib
+
+p = pathlib.Path()
 
 x = np.linspace(1, 5, 5).astype(int)
 y = x
@@ -10,9 +13,8 @@ plt.xlabel("x[s]")
 plt.ylabel("[m]")
 plt.grid()
 plt.legend()
+plt.savefig((p.cwd() / "plot1.pdf").as_posix())
 plt.show()
-f = plt.figure()
-f.set_figheight(val=9 / 2.54)
-f.set_figwidth(val=12 / 2.54)
-
-f.savefig("plot1.pdf")
+fig = plt.gcf()
+fig.set_figheight(val=9 / 2.54)
+fig.set_figwidth(val=12 / 2.54)
