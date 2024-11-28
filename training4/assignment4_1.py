f5eaa616bda0f0904b7840595416b9924f59a7e3
Jorgen Hauge <jorgen78@gmail.com>
2024-11-28 11:56:46 +0100

    Add some training assignments

---
+++ training4/assignment4_1.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pathlib
+
+p = pathlib.Path(__file__).with_name("temp_skien_oo_plotting.pdf")
+filename = p.absolute()
+
+months = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])  # [month no]
+temperatures_c = np.array([-3, -2, 2, 7, 11, 15, 17, 16, 12, 6, 2, -3])  # [C]
+temperatures_f = temperatures_c * (9 / 5) + 32 # [F]
+
+fig = plt.figure()  # Make a figure object with name fig.
+ax = fig.subplots()  # Make an axes object with name ax.
+plt.xlabel("Month number")
+plt.ylabel("Value")
+plt.xticks(months)
+ax.plot(
+    months, temperatures_c, "*-b", label="temp C vs month"
+)  # Use ax.plot() to plot.
+ax.plot(
+    months, temperatures_f, "*-g", label="temp F vs month"
+)  # Use ax.plot() to plot.
+ax.axhline(
+    y=np.nanmean(temperatures_c),
+    label="Mean value of temp C",
+    color="r",
+    linestyle="--",
+)
+ax.axhline(
+    y=np.nanmean(temperatures_f),
+    label="Mean value of temp F",
+    color="c",
+    linestyle="--",
+)
+ax.autoscale(axis="y")
+
+plt.legend()
+plt.savefig(filename)
+plt.show()  # Show the plot with plt.show()
--- training4/assignment4_2.py
+++ training4/assignment4_2.py
@@ -2,19 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pathlib
 
-p = pathlib.Path()
+p = pathlib.Path(__file__).with_name("plot1.pdf")
+filename = p.absolute()
 
 x = np.linspace(1, 5, 5).astype(int)
 y = x
 z = np.sqrt(x)
-plt.plot(x, y, "*-r", label="x vs y")
-plt.plot(x, z, "o-b", label="sqrt(x)")
+fig, axs = plt.subplots(2)
 plt.xlabel("x[s]")
 plt.ylabel("[m]")
-plt.grid()
-plt.legend()
-plt.savefig((p.cwd() / "plot1.pdf").as_posix())
+axs[0].plot(x, y, "*-r", label="x vs y")
+axs[0].grid()
+axs[0].legend()
+axs[1].plot(x, z, "o-b", label="sqrt(x)")
+axs[1].grid()
+axs[1].legend()
+plt.savefig(filename)
 plt.show()
-fig = plt.gcf()
-fig.set_figheight(val=9 / 2.54)
-fig.set_figwidth(val=12 / 2.54)
